a3072c0ebc55550e2e1ba2f35dbe49afc71aa17a
Dmytro Perepolkin <dperepolkin@gmail.com>
2020-06-20 15:59:47 +0200

    Include histogram for eruptions (closes #1)

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,4 @@
 # ShinyCICD (development version)
 
+* The app now also includes histogram for eruptions (#1)
 * Initial example of a packaged Shiny app.
--- R/app_server.R
+++ R/app_server.R
@@ -7,5 +7,6 @@
 app_server <- function(input, output, session) {
 
   callModule(mod_faithful_histogram_server, "hist-waiting", variable=reactive("waiting"))
+  callModule(mod_faithful_histogram_server, "hist-eruptions", variable=reactive("eruptions"))
 
 }
